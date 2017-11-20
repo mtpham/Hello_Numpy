22cb016c3a6b368f2cf0fa42db48ccdae75b580b
Tung Pham <phammanhtung@gmail.com>
2017-11-21 05:19:09 +1000

    numpy_basis with sigmoid func

---
+++ numpy_basis.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1 / (1 + np.exp(-x))
+    return s
+
